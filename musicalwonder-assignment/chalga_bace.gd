85295fb167e9404fd4c8e9492842f90c422163eb
ImSorryJohn <C24730405@myTUDublin.ie>
2024-12-08 07:49:23 +0000

    ON/OFF
    Code for turning music on/off is done just needs implementing everywhere

--- musicalwonder-assignment/chalga_bace.gd
+++ musicalwonder-assignment/chalga_bace.gd
@@ -18,17 +18,40 @@ func _on_return_pressed() -> void:
 func _on_luda_po_tebe_pressed() -> void:
 	$"Luda Po Tebe".play()
 	if $"Luda Po Tebe".playing:
-		$LudaPoTebe.text = "Stop"
+		$LudaPoTebe.text = "Playing"
 		$"Luda Po Tebe".play()
-	if $"Luda Po Tebe".playing:
-		_on_luda_po_tebe_pressed()
-		$"Luda Po Tebe".stop()
 
 
 
 func _on_edno_ferrari_pressed() -> void:
 	$"EDNO FERRARI".play()
+	if $"EDNO FERRARI".playing:
+		$"Edno Ferrari".text = "Playing"
+		$"EDNO FERRARI".play()
 
 
 func _on_gsmgsmgsm_2_pressed() -> void:
 	$GSMGSMGSM.play()
+	if $"GSMGSMGSM".playing:
+		$GSMGSMGSM2.text = "Playing"
+		$"GSMGSMGSM".play()
+
+
+
+
+
+
+
+func _on_stop_slavi_1_pressed() -> void:
+	$"EDNO FERRARI".stop()
+	$"Edno Ferrari".text = "Edno Ferrari"
+
+
+func _on_stop_luda_pressed() -> void:
+	$"Luda Po Tebe".stop()
+	$LudaPoTebe.text = "Luda Po Tebe"
+
+
+func _on_stop_gsmgsmgsm_pressed() -> void:
+	$GSMGSMGSM.stop()
+	$GSMGSMGSM2.text = "GSMGSMGSM"
